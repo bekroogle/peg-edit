cdce059ac0b80963b907d34e93d9fb966d8ee233
Benjamin J. Kruger <bekroogle@gmail.com>
2014-12-16 12:17:16 -0600

    Refactored.

---
+++ js/do-tree.js
@@ -0,0 +1,84 @@
+
+          function doTree() {
+            $('#bottom_right').html('');
+            // ************** Generate the tree diagram  *****************
+            var margin = {top: 80, right: 120, bottom: 20, left: 120},
+              width = 960 - margin.right - margin.left,
+              height = 500 - margin.top - margin.bottom;
+              
+            var i = 0;
+
+            var tree = d3.layout.tree()
+              .size([height, width]);
+
+            var diagonal = d3.svg.diagonal()
+              .projection(function(d) { return [d.x, d.y]; });
+
+            var svg = d3.select("#bottom_right").append("svg")
+              .attr("width", width + margin.right + margin.left)
+              .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top +")")
+                .call(d3.behavior.zoom().scaleExtent([0.5,10]).on("zoom", zoom))
+              .append("g");
+              
+            root = treeData[0];
+              
+            update(root);
+
+            function update(source) {
+
+              // Compute the new tree layout.
+              var nodes = tree.nodes(root).reverse(),
+                links = tree.links(nodes);
+
+              // Normalize for fixed-depth.
+              nodes.forEach(function(d) { d.y = d.depth * 50; });
+
+              // Declare the nodesâ€¦
+              var node = svg.selectAll("g.node")
+                .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+              // Enter the nodes.
+              var nodeEnter = node.enter().append("g")
+                .attr("class", "node")
+                .attr("transform", function(d) { 
+                  return "translate(" + d.x + "," + d.y + ")"; });
+
+              nodeEnter.append("circle")
+                .attr("r", 10)
+                .style("fill", "#fff");
+
+              nodeEnter.append("text")
+                .attr("y", function(d) { 
+                  return d.children || d._children ? -18 : 18; })
+                .attr("dy", ".35em")
+                .attr("text-anchor", "middle")
+                .text(function(d) { return d.name; })
+                .style("fill-opacity", 1);
+
+              // Declare the linksâ€¦
+              var link = svg.selectAll("path.link")
+                .data(links, function(d) { return d.target.id; });
+
+              // Enter the links.
+              link.enter().insert("path", "g")
+                .attr("class", "link")
+                .attr("d", diagonal);
+
+            }
+            // zoom and pan
+            // var zoom = d3.behavior.zoom()
+            // .on("zoom",function() {
+            //     g.attr("transform","translate("+ 
+            //         d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+            //     g.selectAll("path")  
+            //         .attr("d", path.projection(projection)); 
+            // });
+
+            // svg.call(zoom)
+            function zoom() {
+              svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+            }
+
+        };
---
+++ js/doc-ready.js
@@ -0,0 +1,107 @@
+
+        var parser;
+        var treeData;
+        var changeSize = function(target, delta) {
+            target.setOption('fontSize', target.getOption('fontSize') + delta);
+        };
+
+        $('document').ready( function() {
+            // Create the PEG editor
+            editor = ace.edit("editor");
+            editor.setTheme("ace/theme/monokai");
+            editor.getSession().setMode("ace/mode/javascript");
+            
+            // No static JS analysis
+            editor.setOption("useWorker", false);
+            
+            // Personal preferences:
+            editor.setOption("tabSize", 2);
+            editor.setOption('scrollPastEnd',100);
+
+            // Store retrieve text
+            editor.setValue(localStorage["grammar"]);
+            if (editor.getValue !== "") {
+              $('#build_parser_btn').click();
+            }            
+
+            editor.getSession().on("change", function() {
+                localStorage.setItem("grammar", editor.getValue());
+                $('#build_parser_btn').click();
+            });
+
+            // Resize editor
+            $('#editor').css("height", window.innerHeight);
+            editor.resize();
+
+            // Create parse string editor
+            output = ace.edit("output");
+            output.setOption("useWorker", false);
+            output.setValue(localStorage["source"]);
+            output.getSession().on('change', function() {
+                localStorage.setItem("source", output.getValue());
+                $('#parse_btn').click();
+            });
+
+            $('#output').css('height', window.innerHeight / 2);
+            output.resize();
+            $('#build_parser_btn').click( function() {
+                try {
+                    editor.getSession().clearAnnotations();
+                    parser = PEG.buildParser(editor.getValue());
+                    $('.alert-warning').remove();
+                } catch(exn) {
+                  $('#bottom_right').html('<div class="alert alert-warning" role="alert">Grammar Error: '+ exn.message +'</div>');
+                  console.log(exn);
+                  //if (!editor.getSession().$annotations) {
+                    editor.getSession().$annotations = [];
+                  //}
+
+                  var myAnno = {
+                    "column": exn['column'],
+                    "row": exn['line'] - 1,
+                    "type": "error",
+                    "raw": exn['message'],
+                    "text": exn['message']
+                  };
+                
+                  editor.getSession().$annotations.push(myAnno);
+                  editor.getSession().setAnnotations(editor.getSession().$annotations);
+                } // catch(exn)      
+                   
+                
+            });
+
+            $('#parse_btn').click( function() {
+              // If a parser has been built:
+              if (parser) {
+                
+                // Try to parse:
+                try {
+                    result = parser.parse(output.getValue());
+                    treeData = result;
+                    doTree();
+                
+                // Log any parse errors in the console:                    
+                } catch(e) {
+                  $('#bottom_right').html('<div class="alert alert-danger" role="alert">Parse Error: '+ e.message +'</div>');
+                    console.error(e);
+                }
+              }
+            });
+
+            $('#zoom_in_btn').click( function() {
+                changeSize(editor, 2);
+                changeSize(output, 2);
+        
+            });
+            $('#zoom_out_btn').click( function() {
+                changeSize(editor, -2);
+                changeSize(output, -2);
+            })
+            $(output).focus( function() {
+                if (editor.getValue()) {
+                    $('#build_parser_btn').click();
+                }
+            });
+        });
+    
