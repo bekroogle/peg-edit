a6922375a1f448360cc6f7ae95f812a1712cf5a1
Benjamin J. Kruger <bekroogle@gmail.com>
2015-12-06 00:15:16 -0600

    Doing some script debugging.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 rm -rf out || exit 0;
 mkdir out;
 ( cd out
+  echo "git init"
   git init
+  echo "git config user.name "Benjamin J. Kruger""
   git config user.name "Benjamin J. Kruger"
   git config user.email "bekroogle@gmail.com"
   cp ../index.html ./index.html
@@ -10,7 +12,10 @@ mkdir out;
   cp ../stylesheets ./ -r
   cp ../CNAME ./CNAME
   cp ../bower_components ./bower_components -r
+  echo "git add ."
   git add .
+  echo "git commit -m 'Deployed to Github Pages'"
   git commit -m "Deployed to Github Pages"
+  echo "git push --force..."
   git push --force  "https://${secure}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
