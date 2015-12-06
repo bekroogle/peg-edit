7f5527ebfe451ff6e4cf479919b26cae241d04db
Benjamin J. Kruger <bekroogle@gmail.com>
2015-12-05 23:42:31 -0600

    Trying to integrate travis-ci.

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+haml index.haml index.html
+rm -rf out || exit 0;
+mkdir out;
+( cd out
+  git init
+  git config user.name "Benjamin J. Kruger"
+  git config user.email "bekroogle@gmail.com"
+  cp ../index.html ./index.html
+  cp ../js ./ -r
+  cp ../images ./ -r
+  cp ../stylesheets ./ -r
+  cp ../CNAME ./CNAME
+  cp ../bower_components ./bower_components -r
+  git add .
+  git commit -m "Deployed to Github Pages"
+  git push --force --quiet "https://${secure}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
