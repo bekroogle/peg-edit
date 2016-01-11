33db22ec9dce06cca809d85093ba11482397e7c4
Benjamin J. Kruger <bekroogle@gmail.com>
2016-01-11 16:27:47 -0600

    Started jqxDocking.

--- README.md
+++
@@ -1,9 +0,0 @@
-# pegedit
-
-## About
-Pegedit is a web-based development environment for designing PEG grammars (yes, I know that's redundant). It combines [Ace source code editors](http://ace.c9.io) for input with [PEG.js](http://pegjs.org) to generate the parsers and [D3.js](http://d3js.org) to help visualize your parse trees.
-
-## To use
-Visit the website at http://pegedit.cspotrun.org. While I'll do my best to keep the site updated, I don't have any kind of clever build system that deploys my commits, so this repository will always be the place to go for the latest, most authoritative versions.
-
-The [wiki](http://github.com/bekroogle/pegedit/wiki) has some details on building this site from source and should soon contain some decent user information. While the site did have a nice [Joyride](http://foundation.zurb.com/docs/components/joyride.html) walk-through, the UI has changed so much as to make the tips no longer fit.
--- js/app.js
+++ js/app.js
@@ -1,3 +1,80 @@
-// Foundation JavaScript
-// Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+var DOCUMENT_GROUP_WIDTH = window.innerWidth / 2;
+var MENU_WIDTH = window.innerWidth;
+
+$(document).ready(function () {
+  $("#jqxMenu").jqxMenu({ width: MENU_WIDTH, height: 30});
+  // the 'layout' JSON array defines the internal structure of the docking layout
+  var layout = [{
+      type: 'layoutGroup',
+      orientation: 'horizontal',
+      items: [{
+          type: 'autoHideGroup',
+          alignment: 'left',
+          width: 80,
+          unpinnedWidth: 200,
+          items: [{
+              type: 'layoutPanel',
+              title: 'Toolbox',
+              contentContainer: 'ToolboxPanel'
+          }, {
+              type: 'layoutPanel',
+              title: 'Help',
+              contentContainer: 'HelpPanel'
+          }]
+      }, {
+          type: 'layoutGroup',
+          orientation: 'vertical',
+          width: DOCUMENT_GROUP_WIDTH,
+          items: [{
+              type: 'documentGroup',
+              height: 400,
+              minHeight: 200,
+              items: [{
+                  type: 'documentPanel',
+                  title: 'Document 1',
+                  contentContainer: 'Document1Panel'
+              }, {
+                  type: 'documentPanel',
+                  title: 'Document 2',
+                  contentContainer: 'Document2Panel'
+              }]
+          }, {
+              type: 'tabbedGroup',
+              height: 200,
+              pinnedHeight: 30,
+              items: [{
+                  type: 'layoutPanel',
+                  title: 'Error List',
+                  contentContainer: 'ErrorListPanel'
+              }]
+          }]
+      }, {
+          type: 'tabbedGroup',
+          width: 220,
+          minWidth: 200,
+          items: [{
+              type: 'layoutPanel',
+              title: 'Solution Explorer',
+              contentContainer: 'SolutionExplorerPanel'
+          }, {
+              type: 'layoutPanel',
+              title: 'Properties',
+              contentContainer: 'PropertiesPanel'
+          }]
+      }]
+  }, {
+      type: 'floatGroup',
+      width: 200,
+      height: 200,
+      position: {
+          x: 150,
+          y: 150
+      },
+      items: [{
+          type: 'layoutPanel',
+          title: 'Output',
+          contentContainer: 'OutputPanel'
+      }]
+  }];
+  $('#jqxDockingLayout').jqxDockingLayout({ width: window.innerWidth, height: window.innerHeight, layout: layout });
+});
